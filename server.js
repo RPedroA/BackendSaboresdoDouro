879d13d9b004aaf254b55f4fbca9b23c0d211ce5
RPedroA <r.u.a@ipvc.pt>
2025-01-07 23:21:21 +0000

    ping route

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ app.use(cors());
 
 app.use(router);
 
+app.get("/ping", (req, res) => {
+  res.status(200).json({ message: ":) O Servidor esta a funcionar." });
+});
+
 const port = process.env.SERVER_PORT || 8080;
 
 app.listen(port, () => console.log(`Express server running on port ${port}`));
 
-//falta adicionar 
+//falta adicionar
 //filtros
 //limites de caracteres para descrição nomes etc
 //falta logout adiionar a parte da frontend
